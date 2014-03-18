aa611ca58d81feac80d11666c9de4d0a0f9d6c5c
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-03-18 21:13:53 +0100

    Use enum34 for enumeration types. Event object.

---
+++ src/geoffrey/namespace.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Namespace(Enum):
+    filesystem = "filesystem"
+    server = "server"
+    plugin = "plugin"
+    project = "project"
+    metric = "metric"
+    version = "version"
