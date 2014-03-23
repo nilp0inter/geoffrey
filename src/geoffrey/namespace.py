4db761b10cdc12f4da429d1c82aca52e7328f953
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-03-23 21:07:39 +0100

    Cleanup for new server.

--- src/geoffrey/namespace.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-class Namespace(Enum):
-    filesystem = "filesystem"
-    server = "server"
-    plugin = "plugin"
-    project = "project"
-    metric = "metric"
-    version = "version"
