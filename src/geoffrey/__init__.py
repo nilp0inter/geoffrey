12d9f389cb663e67960ceec16feb53ba5b6b5faf
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-03-26 21:11:58 +0100

    Main function with an asyncio loop that run forever. Exists on Ctrl-C. Closes #2 and closes #4.

--- src/geoffrey/__init__.py
+++ src/geoffrey/__init__.py
@@ -1,3 +1,15 @@
-"""
+import asyncio
+import signal
 
-"""
+loop = asyncio.get_event_loop()
+
+
+def handle_ctrl_c():
+    # TODO: Use logging
+    print("Exiting...")
+    loop.stop()
+
+
+def main():
+    loop.add_signal_handler(signal.SIGINT, handle_ctrl_c)
+    loop.run_forever()
