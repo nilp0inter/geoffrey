67b3ed4249428c186a9ab0bda170cec99cedbb2c
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-03-26 21:45:43 +0100

    Read main configuration file. Closes #15.

--- src/geoffrey/__init__.py
+++ src/geoffrey/__init__.py
@@ -1,7 +1,12 @@
 import asyncio
+import configparser
+import os
 import signal
 
 loop = asyncio.get_event_loop()
+DEFAULT_CONFIG_FILENAME = os.path.join(os.path.expanduser('~'),
+                                       '.geoffrey',
+                                       'geoffrey.conf')
 
 
 def handle_ctrl_c():
@@ -11,5 +16,7 @@ def handle_ctrl_c():
 
 
 def main():
+    config = configparser.ConfigParser()
+    config.read(DEFAULT_CONFIG_FILENAME)
     loop.add_signal_handler(signal.SIGINT, handle_ctrl_c)
     loop.run_forever()
