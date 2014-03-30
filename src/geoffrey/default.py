fc9451a129395e61720da1d74b555954433f5f2f
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-03-30 23:56:57 +0200

    Removed unused files. Closes #26.

--- src/geoffrey/default.py
+++
@@ -1,6 +0,0 @@
-import os
-
-HOST = '127.0.0.1'
-WEBSERVER_PORT = 8888
-WEBSOCKET_PORT = 8765
-GEOFFREY_RC_FILE = os.path.join(os.path.expanduser('~'), '.geoffreyrc')
--- src/geoffrey/plugins/__init__.py
+++
@@ -1,2 +0,0 @@
-# Namespace package
-# KEEP EMPTY
