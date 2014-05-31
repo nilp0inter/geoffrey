84453e7b17669362216736a3ba81d20667f261d4
Miguel Reguero Bejar <mreguerob0@gmail.com>
2014-05-31 20:19:20 +0200

    Funcion slugify añadida

--- src/geoffrey/utils.py
+++ src/geoffrey/utils.py
@@ -1,3 +1,16 @@
+import re
+import uuid
+
+alphanumeric = re.compile(r'[\x80-\xFF(\W+)]')
+
+
 def write_template(filename, content):
     with open(filename, 'w', encoding='utf-8') as f:
         f.write(content)
+
+
+def slugify(text):
+    slugified = alphanumeric.sub('_', text.lower().rstrip())
+    if not slugified:
+        slugified = str(uuid.uuid4())
+    return slugified
