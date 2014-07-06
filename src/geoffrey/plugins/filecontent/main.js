b648b749dd0b4d2ace4314a19a54ff135a8ff94a
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-07-06 20:20:27 +0200

    filecontent client plugin refactor.

--- src/geoffrey/plugins/filecontent/main.js
+++ src/geoffrey/plugins/filecontent/main.js
@@ -1,131 +1,133 @@
 $(function() {
+  var FilecontentModel = Backbone.Model.extend({
+  });
+
   var FilecontentView = Backbone.View.extend({
     id: "filecontent",
 
     initialize: function() {
+      this.model = new FilecontentModel({});
+
       loadCSS('/assets/libs/codemirror/theme/blackboard.css');
-      this.listenTo(window.app, 'route', this.updateSubscriptions);
+
+      this.listenTo(window.app, 'route', this.managePanel);
+      this.listenTo(this.model, 'change', this.fileChange);
     },
 
-    openNode: function(nodename) {
+    fileChange: function() {
+      var this_ = this;
+
+      window.app.subscribe(
+        'update_filecontent_highlights',
+        {
+          'project': project_id,
+          'content_type': 'highlight',
+          'key': this.model.get("filename") 
+        },
+        function(data) {
+          // Force codemirror to recheck for errors.
+          this_.refreshEditor(data.key);
+        }
+      ); 
+    },
+
+    openNode: function(filename) {
+      /* Open this file on jstree. */
+      this.model.set("filename", filename);
       this.tree.jstree("deselect_all");
-      this.tree.jstree("open_node", nodename);
-      this.tree.jstree("select_node", nodename);
+      this.tree.jstree("open_node", filename);
+      this.tree.jstree("select_node", filename);
     },
 
-    refreshEditor: function() {
-      // Force codemirror to recheck for errors.
-      this_.editor.setValue(this_.editor.getValue());
+    refreshEditor: function(filename) {
+      /* Force codemirror to recheck for errors. */
+      var this_ = this;
+      var codeUrl = "/api/v1/states?" + $.param({
+        project: project_id,
+        plugin: "filecontent",
+        task: "read_modified_files",
+        key: filename
+      });
+      function setData(data) {
+        /* Set data on the editor */
+        if (data.length) {
+          this_.editor.getDoc().setValue(data[0].value.content);
+
+          var modes = CodeMirror.modeInfo.filter(
+            function(mode){
+              return mode.mime==data[0].value.mime_type
+            }
+          );
+    
+          if(modes.length) {
+            this_.editor.setOption("mode", modes[0].mode);
+            CodeMirror.autoLoadMode(this_.editor, modes[0].mode); 
+          } else {
+            this_.editor.setOption("mode", "null");
+          }
+        }
+      }
+      $.get(codeUrl, setData); // Set's the code async.
     },
 
     setUp: function() {
-      // Subscribe
-      // Bind events
-      // Open node
-    },
-
-    tearDown: function() {
-      // Unsubscribe
-      // Unbind events
-    },
-
-    open_file: function(nodename) {
-      this.tree.jstree("deselect_all");
-      this.tree.jstree("open_node", nodename);
-      this.tree.jstree("select_node", nodename);
-    },
-
-    createTree: function(node) {
+      /* Setup the filecontent view */
       var this_ = this;
 
-      if(this.tree){
-        if(this.tree.jstree().refresh){
-          this.tree.jstree().refresh();
-        } else {
-          this.tree.off();
-          this.tree = null;
-          this.createTree(node);
-          return;
-        }
-      } else {
-          var tree = $("#tree").jstree({
-            'core': {
-              'multiple': false,
-              'data': {
-                'url': function (node) {
-                  return '/api/v1/' + project_id + '/filecontent/method/filetree';
-                },
-                'data' : function (node) {
-                  return { 'id' : node.id };
-                }
-              }
+      // Create jsTree tree & CodeMirror editor
+      this.tree = $("#tree").jstree({
+        'core': {
+          'multiple': false,
+          'data': {
+            'url': function (node) {
+              return '/api/v1/' + project_id + '/filecontent/method/filetree';
+            },
+            'data' : function (node) {
+              return { 'id' : node.id };
             }
-          });
-
-          tree.off();
-          $("#tree").on("select_node.jstree", function (e, data) {
-            var filename = data.selected[0];
-            this_.subscribeHighlights(filename);
-            this_.filename = filename;
-
-            $.get( "/api/v1/states?project=" + project_id + "&plugin=filecontent&task=read_modified_files&key=" + filename, function( data ) {
-              if (data.length) {
-                this_.editor.getDoc().setValue(data[0].value.content);
-
-                var modes = CodeMirror.modeInfo.filter(function(mode){ return mode.mime==data[0].value.mime_type})
-      
-                if(modes.length) {
-                  this_.editor.setOption("mode", modes[0].mode);
-                  CodeMirror.autoLoadMode(this_.editor, modes[0].mode); 
-                } else {
-                  this_.editor.setOption("mode", "null");
-                }
-              }
-            }); 
-          });
-
-        this.tree = tree;
-
-      }
-
-      this.tree.on("refresh.jstree", function (e, data) {
-        this_.open_file(node);
+          }
+        }
       });
-      this.tree.on("ready.jstree", function (e, data) {
-        this_.open_file(node);
-      });
-
-    },
-
-    createEditor: function() {
-      var this_ = this;
 
       CodeMirror.modeURL="/assets/libs/codemirror/mode/%N/%N.js";
 
-      function get_highlights(cm, updateLinting, options) {
-        if (this_.filename) {
-          $.get("/api/v1/states?project=" + project_id + "&content_type=highlight&key=" + this_.filename, function(data) {
-              var highlights = [];
-              for(var p=0; p<data.length; p++) {
-                  var hl = data[p].value.highlights;
-                  for(var v=0; v<hl.length; v++){
-                      var msg = hl[v];
-                      highlights.push({
-                          from: {line: msg.start_line-1, chr: msg.start_char},
-                          to: {line: msg.end_line-1, chr: msg.end_char},
-                          message: '[' + data[p].plugin + '] ' + msg.text,
-                          severity: msg.type
-                      });
-                  }
-              }
-              
-              updateLinting(cm, highlights);
+      function updateHighlights(cm, updateLinting, options) {
+        /* 
+           Update highlights on CodeMirror.
+           This method is called by CodeMirror's lint plugin.
+        */
+        if (this_.model.get("filename")) {
+          var highlightUrl = "/api/v1/states?" + $.param({
+            project: project_id,
+            content_type: "highlight",
+            key: this_.model.get("filename")
           });
+
+          function setHighlights(data) {
+            /*
+               Transform the Geoffrey's highlight convention to CodeMirror
+               highlights and calls `updateLinting`.
+            */
+            var highlights = [];
+            for(var p=0; p<data.length; p++) {
+                var hl = data[p].value.highlights;
+                for(var v=0; v<hl.length; v++){
+                    var msg = hl[v];
+                    highlights.push({
+                        from: {line: msg.start_line-1, chr: msg.start_char},
+                        to: {line: msg.end_line-1, chr: msg.end_char},
+                        message: '[' + data[p].plugin + '] ' + msg.text,
+                        severity: msg.type
+                    });
+                }
+            }
+            updateLinting(cm, highlights);
+          }
+          $.get(highlightUrl, setHighlights); // Set's the highlights async.
         }
-        return;
       }
 
-      var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+      this.editor = CodeMirror.fromTextArea(document.getElementById("code"), {
         theme: 'blackboard',
         path: "/assets/libs/codemirror/",
         lineNumbers: true,
@@ -134,61 +136,67 @@ $(function() {
         readOnly: false,
         gutters: ["CodeMirror-lint-markers"],
         lint: {
-          "getAnnotations": get_highlights,
+          "getAnnotations": updateHighlights,
           "async": true,
         }
       });
 
-      this.editor = editor;
+      // Subscribe
+      window.app.subscribe(
+        'update_filecontent_files',
+        {
+          'project': project_id,
+          'content_type': 'data',
+          'plugin': 'filecontent'
+        },
+
+        function(data) {
+          $("#tree").jstree("deselect_all");
+          
+          $("#tree").jstree().refresh();
+          this_.model.set("filename", data.key);
+        }
+      ); 
+
+      this.fileChange();
+
+      // Bind events
+      this.tree.on("select_node.jstree", function (e, data) {
+        this_.model.set("filename", data.selected[0]);
+        this_.refreshEditor(data.selected[0]);
+      });
+
+      // Open node
+      this.tree.on("ready.jstree", function (e, data) {
+        this_.openNode(this_.model.get("filename"));
+      });
+      this.tree.on("refresh.jstree", function (e, data) {
+        this_.openNode(this_.model.get("filename"));
+      });
+    },
+
+    tearDown: function() {
+      // Unsubscribe
+      window.app.unsubscribe('update_filecontent_files'); 
+      window.app.unsubscribe('update_filecontent_highlights'); 
+
+      // Unbind events
+      this.tree.off("select_node.jstree");
     },
 
     render: function() {
       this.$el.addClass("row");
       this.$el.html('<div id="editor" class="col-md-9" style="min-height: 777px"><textarea id="code" cols="120" rows="30" name="foo"></textarea></div></div><div id="tree" class="col-md-3"></div>');
-
-      this.createEditor();
-      this.createTree();
-
     },
 
-    updateSubscriptions: function(route, params) {
-      var this_ = this;
+    managePanel: function(route, params) {
+      /* Setup or teardown this panel based on current route */
       if(params[0] == "filecontent") {  // Enter this panel, active subscriptions.
-        window.app.subscribe(
-          'update_filecontent_files',
-          {
-            'project': project_id,
-            'content_type': 'data',
-            'plugin': 'filecontent'
-          },
-          function(data) {
-            console.log("Received " + data.key);
-            this_.filename = data.key;
-            this_.createTree(data.key);
-          }); 
-
+        this.setUp()
       } else {
-        window.app.unsubscribe('update_filecontent_files'); 
-        window.app.unsubscribe('update_filecontent_highlights'); 
+        this.tearDown()
       }
     },
-
-    subscribeHighlights: function(filename) {
-      var this_ = this;
-        window.app.subscribe(
-          'update_filecontent_highlights',
-          {
-            'project': project_id,
-            'content_type': 'highlight',
-            'key': filename
-          },
-          function(data) {
-            // Force codemirror to recheck for errors.
-            this_.editor.setValue(this_.editor.getValue());
-          }
-        ); 
-    }
-
   });
 
   var filecontent_panel = new FilecontentView();
@@ -200,10 +208,9 @@ $(function() {
      icon: "fa-file-text-o",
      active: false,
      content: filecontent_panel,
-    });
+  });
 
   window.app.registerMenu(entry);
   window.fc = filecontent_panel;
 
 });
-
